e597a7e62fbe891c8b4fc384da925a29c3a3baff
Gustavo Antonio Pavan <gustavopavan993@gmail.com>
2026-03-24 20:08:44 -0300

    Initial commit

---
+++ src/main/java/com/example/pipeup/PipeUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipeUpApplication.class, args);
+	}
+
+}
