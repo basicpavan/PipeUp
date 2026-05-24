4b3b7f28a31944245d223059340178f404da8200
Marcos Engelmann <engelmannmarcos290@gmail.com>
2026-05-24 20:15:04 -0300

    fix: restaura arquivos corrompidos pelo OneDrive

---
+++ src/main/java/com/example/pipeup/repository/TarefaRepository.java
@@ -0,0 +1,20 @@
+package com.example.pipeup.repository;
+
+import com.example.pipeup.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Integer> {
+
+    List<Tarefa> findByDescricaoContaining(String filtro);
+
+    @Query(value = """
+        SELECT t.*
+        FROM tarefa t
+        INNER JOIN tarefa_usuario tu ON tu.tarefa_id = t.id_tarefa
+        WHERE tu.usuario_id = :usuarioId
+        """, nativeQuery = true)
+    List<Tarefa> findTarefasByUsuarioId(Integer usuarioId);
+}
