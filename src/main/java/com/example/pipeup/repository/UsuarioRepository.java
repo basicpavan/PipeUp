961fea9b5e1feeb426767a22321b6e64b9fcc001
apololbecker <apololuizbecker@outlook.com>
2026-06-03 23:49:05 -0300

    Ajuste usuario repository

--- src/main/java/com/example/pipeup/repository/UsuarioRepository.java
+++ src/main/java/com/example/pipeup/repository/UsuarioRepository.java
@@ -2,17 +2,6 @@ package com.example.pipeup.repository;
 
 import com.example.pipeup.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-import org.springframework.stereotype.Repository;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String email);
-    List<Usuario> findAllByOrderByCriadoEmDesc();
-}
-=======
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -22,4 +11,3 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
     @Query("SELECT u FROM Tarefa t JOIN t.responsaveis u WHERE t.id = :tarefaId")
     List<Usuario> findUsuariosByTarefaId(Integer tarefaId);
 }
->>>>>>> 66aac797dcb5269ffa00671f93e736700f0dd81f
